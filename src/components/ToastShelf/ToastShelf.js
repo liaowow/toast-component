4cb221912733e1fdbced3b26325acec878d6343c
Annie Liao <5109918+liaowow@users.noreply.github.com>
2023-05-15 11:07:11 -0400

    cAlong: Update ToastShelf with toasts prop

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -3,15 +3,14 @@ import React from 'react';
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
-function ToastShelf() {
+function ToastShelf({ toasts }) {
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toasts.map((toast) => (
+        <li className={styles.toastWrapper} key={toast.id}>
+          <Toast variant={toast.variant}>{toast.message}</Toast>
+        </li>
+      ))}
     </ol>
   );
 }
