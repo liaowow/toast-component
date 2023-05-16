99080c4e199b821cb2912d6e1a8c9c21b057d92a
Annie Liao <5109918+liaowow@users.noreply.github.com>
2023-05-16 14:29:58 -0400

    cAlong: Add event handler for Escape key to clear toasts

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -23,6 +23,20 @@ function ToastProvider({ children }) {
     setToasts(nextToasts);
   }
 
+  React.useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+  }, []);
+
   return (
     <ToastContext.Provider value={{ toasts, createToast, handleDismiss }}>
       {children}
